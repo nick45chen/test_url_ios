bef427af016a747cc2b7f0818ca187ed60b4f233
nick chen <nick45chen@gmail.com>
2022-11-14 00:25:05 +0800

    Initial Commit

---
+++ test_url_connect/test_url_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_url_connectApp.swift
+//  test_url_connect
+//
+//  Created by Nick Chen on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct test_url_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_url_connectUITests/test_url_connectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_url_connectUITestsLaunchTests.swift
+//  test_url_connectUITests
+//
+//  Created by Nick Chen on 2022/11/14.
+//
+
+import XCTest
+
+final class test_url_connectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
